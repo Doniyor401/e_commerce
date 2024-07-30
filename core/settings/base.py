550b9ac5d1932f4d7a3f7f4ea63fd0b27729d47d
Doniyor Tursunboyev <doniyortursunboyev000@bk.ru>
2024-07-30 15:20:22 +0500

    fix: staticfiles

--- core/settings/base.py
+++ core/settings/base.py
@@ -144,7 +144,9 @@ USE_TZ = True
 
 # STATIC URLS
 STATIC_URL = 'static/'
-STATICFILES_DIRS = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
